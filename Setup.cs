3bb3ff77948079585ad9677c271fc3297e5db9ee
micha <micha@ORZO>
2025-01-17 22:54:00 +0100

    Add project files.

---
+++ Setup.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class Setup
+    {
+        private IWebDriver driver = null!;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--start-maximized"); 
+            driver = new ChromeDriver(options);
+        }
+
+        [Test]
+        public void OpenGoogleTest()
+        {
+            driver.Navigate().GoToUrl("https://demoqa.com/");
+            Assert.That(driver.Title, Is.EqualTo("DEMOQA"));
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            if (driver != null)
+            {
+                driver.Quit();
+            }
+        }
+    }
+}
