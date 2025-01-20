b9016e36283438be228472b55ec62675f26218d8
micha <micha@ORZO>
2025-01-20 23:43:49 +0100

    Removed usings

--- Automation/Pages/Elements/Actions/ElementsPageActions.cs
+++ Automation/Pages/Elements/Actions/ElementsPageActions.cs
@@ -1,10 +1,6 @@
 ﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
-
-
 
 namespace SeleniumProject.Automation.Pages.Elements.Actions
 {
